74ea5f5ba13a15ff6fcb769b6c1fc130d6c64ab4
Mikołaj Urbański <m.urbanski@goldensubmarine.com>
2023-07-11 11:26:32 +0200

    init

---
+++ server/index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser"
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE);
+        console.log("Connected to database.")
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
+
+app.use(cors({credentials: true, origin: 'http://localhost:5173'}));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use((error, req, res, next) => {
+    const errorStatus = error.status || 500;
+    const errorMessage = error.message || "Something went wrong";
+
+    return res.status(errorStatus).json(errorMessage);
+});
+
+app.listen(8800, () => {
+    connect();
+    console.log("Server is running.");
+});
