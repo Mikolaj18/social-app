a739febc6cafbd13fa8680c891f515ebbd1ca403
Mikołaj Urbański <m.urbanski@goldensubmarine.com>
2023-07-26 19:12:35 +0200

    Fetch friends list to the menu

--- client/src/components/RightMenu/RightMenu.jsx
+++ client/src/components/RightMenu/RightMenu.jsx
@@ -1,47 +1,32 @@
 import "./rightMenu.scss";
+import {useQuery} from "@tanstack/react-query";
+import {getFriendsList} from "../../db/friends/getFriendsList.js";
+import {useContext} from "react";
+import {AuthContext} from "../../context/authContext.jsx";
+import Spinner from "../Spinner/Spinner.jsx";
 
 const RightMenu = () => {
+    const {currentUser} = useContext(AuthContext);
+
+    const {isLoading, error, data} = useQuery({
+        queryKey: [currentUser._id],
+        queryFn: () => getFriendsList(currentUser._id),
+    });
+
+    console.log(data);
     return (
         <aside className="rightMenu">
             <div className="rightMenu__wrapper">
                 <h2 className="rightMenu__title">Contacts</h2>
                 <ul className="rightMenu__list">
-                    <li className="rightMenu__list-item">
-                        <img src= "../src/images/default.jpg" alt="Profile picture"/>
-                        <p>John Doe</p>
-                    </li>
-                    <li className="rightMenu__list-item">
-                        <img src= "../src/images/default.jpg" alt="Profile picture"/>
-                        <p>John Doe</p>
-                    </li>
-                    <li className="rightMenu__list-item">
-                        <img src= "../src/images/default.jpg" alt="Profile picture"/>
-                        <p>John Doe</p>
-                    </li>
-                    <li className="rightMenu__list-item">
-                        <img src= "../src/images/default.jpg" alt="Profile picture"/>
-                        <p>John Doe</p>
-                    </li>
-                    <li className="rightMenu__list-item">
-                        <img src= "../src/images/default.jpg" alt="Profile picture"/>
-                        <p>John Doe</p>
-                    </li>
-                    <li className="rightMenu__list-item">
-                        <img src= "../src/images/default.jpg" alt="Profile picture"/>
-                        <p>John Doe</p>
-                    </li>
-                    <li className="rightMenu__list-item">
-                        <img src= "../src/images/default.jpg" alt="Profile picture"/>
-                        <p>John Doe</p>
-                    </li>
-                    <li className="rightMenu__list-item">
-                        <img src= "../src/images/default.jpg" alt="Profile picture"/>
-                        <p>John Doe</p>
-                    </li>
-                    <li className="rightMenu__list-item">
-                        <img src= "../src/images/default.jpg" alt="Profile picture"/>
-                        <p>John Doe</p>
-                    </li>
+                    {isLoading ? <Spinner/> : error ? "Something went wrong" :
+                        data.map(f => (
+                            <li className="rightMenu__list-item" key={f._id}>
+                                <img src={f.profilePicture ? f.profilePicture : "../src/images/default.jpg"} alt="Profile picture"/>
+                                <p>{f.name} {f.surname}</p>
+                            </li>
+                        ))
+                    }
                 </ul>
             </div>
         </aside>
